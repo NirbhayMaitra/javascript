f1941ad439cab65c8514f63c6d04a65434749439
Nirbhay Maitra <60140561+NirbhayMaitra@users.noreply.github.com>
2024-05-13 07:16:41 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+/*
+Javascript engine is hidden in browser
+alert(3+3) => This statement won't work here because we are using nodejs not a browser
+//code readability should be hoigh
+*/
+
+let name = "Priyanca";
+let age = 24;
+let isLoggedIn = false;
+let state;
+
+
+/* Primitive Data Types*/
+//number => 2 to the power 53
+//bigInt
+//string = ""
+//boolean = true/false
+//null (standalone value) represantion of empty value
+//undefined => value is not assigned yet
+//symbol => unique
+
+
+//object
+console.log(typeof "priyanca")
+console.log(typeof age)
+console.log(typeof null)     /* ***IMPORTANT  */
+console.log(typeof undefined)
