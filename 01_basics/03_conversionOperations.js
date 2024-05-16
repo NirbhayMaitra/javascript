ae07cdcb3ed0a90f736d0dab9350fbf537caeabf
Nirbhay Maitra <60140561+NirbhayMaitra@users.noreply.github.com>
2024-05-16 07:05:29 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,62 @@
+// let score = "33abc";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let con = Number(score);
+// console.log(typeof con)
+// console.log(con);
+
+
+
+// let score_1 = null;
+// console.log(typeof score_1);
+
+// let con_1 = Number(score_1);
+// console.log(typeof con_1);
+// console.log(con_1);
+
+
+
+
+// let score_2 = undefined;
+// console.log(typeof score_2);
+
+// let con_2 = Number(score_2);
+// console.log(typeof con_2);
+// console.log(con_2);
+
+
+
+
+// let score_3 = false;
+// console.log(typeof score_3);
+
+// let con_3 = Number(score_3);
+// console.log(typeof con_3);
+// console.log(con_3);
+
+
+// let score_4 = "Priyanca";
+// console.log(typeof score_4);
+
+// let con_4 = Number(score_4);
+// console.log(typeof con_4);
+// console.log(con_4);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 and false => 0
+
+// let isLoggedIn = "";
+// let conversion = Boolean(isLoggedIn);
+// console.log(conversion)
+
+// "" => false   "xyz" => true
+// 1 => true     0 => false
+
+
+let number = 33;
+let conversion = String(number);
+console.log(typeof conversion);
+
