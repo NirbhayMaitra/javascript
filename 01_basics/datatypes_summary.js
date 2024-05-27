ece0ed487241133604748391789c3e89a6ee33f1
Nirbhay Maitra <60140561+NirbhayMaitra@users.noreply.github.com>
2024-05-27 11:45:12 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,67 @@
+/* 
+Primitive (Call By Value)
+7 types: String, Number, Boolean, Null, undefined, Symbol, BigInt
+
+Non-Primitive (Call By Reference)
+Arrays, Objects, Functions 
+
+JavaScript is a dynamically typed language. This means that types are associated with values rather than variables, 
+and the type of a variable is determined at runtime. Here are some key characteristics of dynamically typed languages like JavaScript:
+
+Type Inference at Runtime: The type of a variable is determined when the code is executed, not when it is written.
+Flexibility: Variables can hold values of any type and can be reassigned to different types over their lifetime.
+No Explicit Type Declarations: You do not need to declare the type of a variable when you create it.
+For example, in JavaScript, you can write:
+
+javascript
+Copy code
+let x = 42;    // x is a number
+x = "Hello";   // Now x is a string
+x = true;      // Now x is a boolean
+This flexibility contrasts with statically typed languages, where the type of a variable is known at compile time and usually must be explicitly declared and cannot change. Examples of statically typed languages include Java, C++, and TypeScript (which is a statically typed superset of JavaScript).
+
+*/
+
+
+
+
+
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id === anotherId)
+
+const bigInt = 2349023940234903933024203492850923902n;
+
+
+
+
+
+const airlines = ["Emirates", "Qatar", "Air India Express"]
+const myObj = {
+    name: "axzsdc",
+    age: 25
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+console.log(typeof score)
+console.log(typeof scoreValue)
+console.log(typeof isLoggedIn)
+console.log(typeof outsideTemp)
+console.log(typeof id)
+console.log(typeof anotherId)
+console.log(typeof bigInt)
+console.log(typeof airlines)
+console.log(typeof myObj)
+console.log(typeof myFunction)
